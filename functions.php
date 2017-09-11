50bbbd3ea15127bc1991a1bb81370b41e157063f
Nate Finch <finchproservices@gmail.com>
2017-09-10 20:01:34 -0500

    edits to theme

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
 
 // Set Localization (do not remove).
 add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
-function genesis_sample_localization_setup(){
+function genesis_sample_localization_setup() {
 	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
 }
 
@@ -49,7 +49,7 @@ define( 'CHILD_THEME_VERSION', '2.3.0' );
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-google-fonts', '//fonts.googleapis.com/css?family=Raleway|Roboto+Slab', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
@@ -87,7 +87,14 @@ function genesis_sample_responsive_menu_settings() {
 add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
 
 // Add Accessibility support.
-add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+add_theme_support( 'genesis-accessibility', array(
+	'404-page',
+	'drop-down-menu',
+	'headings',
+	'rems',
+	'search-form',
+	'skip-links'
+) );
 
 // Add viewport meta tag for mobile browsers.
 add_theme_support( 'genesis-responsive-viewport' );
@@ -111,10 +118,13 @@ add_theme_support( 'genesis-after-entry-widget-area' );
 add_theme_support( 'genesis-footer-widgets', 3 );
 
 // Add Image Sizes.
-add_image_size( 'featured-image', 720, 400, TRUE );
+add_image_size( 'featured-image', 720, 400, true );
 
 // Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+add_theme_support( 'genesis-menus', array(
+	'primary'   => __( 'After Header Menu', 'genesis-sample' ),
+	'secondary' => __( 'Footer Menu', 'genesis-sample' )
+) );
 
 // Reposition the secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
@@ -148,4 +158,30 @@ function genesis_sample_comments_gravatar( $args ) {
 
 	return $args;
 
+}
+
+
+
+//* Modify the header URL - HTML5 Version
+add_filter( 'genesis_seo_title', 'child_header_title', 10, 3 );
+function child_header_title( $title, $inside, $wrap ) {
+
+	$blog_url = get_bloginfo( 'url' );
+
+    $inside = sprintf( '<a href="' . $blog_url . '" title="%s">%s</a>', esc_attr( get_bloginfo( 'name' ) ), 'Alex <span class="last-name">Gee</span>' );
+
+    return sprintf( '<%1$s class="site-title">%2$s</%1$s>', $wrap, $inside );
+}
+
+
+//* Add Featured image to posts
+
+add_action( 'genesis_before_entry_content', 'gee_add_featured_image' );
+
+function gee_add_featured_image() {
+	?>
+	<div class="post-featured-image">
+		<?php the_post_thumbnail( $post_id, 'featured-image' ); ?>
+	</div>
+	<?php
 }
